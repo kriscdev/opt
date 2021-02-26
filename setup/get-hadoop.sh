217e1777c41e3f15d0851cffece7d75cff7d7f81
kriscdev <60680627+kriscdev@users.noreply.github.com>
2021-02-26 01:41:11 -0500

    Create get-hadoop.sh

---
+++ setup/get-hadoop.sh
@@ -0,0 +1,4 @@
+wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+tar xvf hadoop-3.3.0.tar.gz
+rm hadoop-3.3.0.tar.gz
+mv hadoop-3.3.0 /opt/hadoop
