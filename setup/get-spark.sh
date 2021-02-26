ff1da55cad1d9b77da387ba6b43760abc2cfac70
kriscdev <60680627+kriscdev@users.noreply.github.com>
2021-02-26 01:39:28 -0500

    Create get-spark.sh

---
+++ setup/get-spark.sh
@@ -0,0 +1,4 @@
+wget https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop3.2.tgz
+tar xvf spark-3.0.2/spark-3.0.2-bin-hadoop3.2.tgz
+rm spark-3.0.2/spark-3.0.2-bin-hadoop3.2.tgz
+mv spark-3.0.2-bin-hadoop3.2 $DEV_HOME/spark
