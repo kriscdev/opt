7df2fe60d93a4f5b8cd0f00373ce10addc39fda2
kriscdev <60680627+kriscdev@users.noreply.github.com>
2021-03-10 20:46:03 -0500

    Update get-spark.sh

--- setup/get-spark.sh
+++ setup/get-spark.sh
@@ -1,4 +1,4 @@
 wget https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop3.2.tgz
 tar xvf spark-3.0.2-bin-hadoop3.2.tgz
 rm spark-3.0.2-bin-hadoop3.2.tgz
-mv spark-3.0.2-bin-hadoop3.2 opt/spark
+mv spark-3.0.2-bin-hadoop3.2 /opt/spark
