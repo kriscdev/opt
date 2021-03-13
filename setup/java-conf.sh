00645f2f6b1e1289a16921a17ea3b64b8afb86dd
kriscdev <60680627+kriscdev@users.noreply.github.com>
2021-03-12 22:34:20 -0500

    Create java-conf.sh

---
+++ setup/java-conf.sh
@@ -0,0 +1,9 @@
+JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk
+SPARK_HOME=/opt/spark
+JAVA=/usr/lib/jvm/java-1.11.0-openjdk/bin/java
+JAVAC=/usr/lib/jvm/java-1.11.0-openjdk/bin/javac
+JAVAH=/usr/share/aclocal/ax_prog_javah.m4
+JAR=/usr/lib/jvm/java-1.11.0-openjdk/bin/jar
+JAVA_LIBS='-L/usr/lib/jvm/java-1.11.0-openjdk/lib/server -ljvm'
+JAVA_CPPFLAGS='-I/usr/lib/jvm/java-1.11.0-openjdk/include -I/usr/lib/jvm/java-1.11.0-openjdk/include/linux'
+JAVA_LD_LIBRARY_PATH=/usr/lib/jvm/java-1.11.0-openjdk/lib/server
