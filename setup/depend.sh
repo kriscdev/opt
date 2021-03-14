0c67c28f29ae5cc85fcd448e6bdef71cbe8ed9b3
kriscdev <60680627+kriscdev@users.noreply.github.com>
2021-03-13 23:25:23 -0500

    Update depend.sh

--- setup/depend.sh
+++ setup/depend.sh
@@ -1,3 +1,4 @@
-swupd bundle-add java-basic buildreq-spark R-extras
+swupd bundle-add java-basic java-runtime buildreq-spark R-extras
 R CMD JAVARECONF
-Rscript -e "install.packages(c('rJava','sparklyr', 'IRkernel', 'tm', 'openNLP', 'RWeka', 'shiny', 'officer', 'rio', 'knitr', 'rmarkdown', 'devtools', 'testthat', 'e1071', 'survival', 'ggplot2', 'mplot', 'googleVis','glmnet', 'pROC', 'data.table', 'caret', 'sqldf', 'wordcloud'), repos='https://cloud.r-project.org/')"
+Rscript -e "install.packages(c('rJava'), repos='https://cloud.r-project.org/')"
+#Rscript -e "install.packages(c('rJava','sparklyr', 'IRkernel', 'tm', 'openNLP', 'RWeka', 'shiny', 'officer', 'rio', 'knitr', 'rmarkdown', 'devtools', 'testthat', 'e1071', 'survival', 'ggplot2', 'mplot', 'googleVis','glmnet', 'pROC', 'data.table', 'caret', 'sqldf', 'wordcloud'), repos='https://cloud.r-project.org/')"
